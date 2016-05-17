9ca098e4ea72c19d0fe2ad0a8792621f9e700236
M!nified <gora.pub@gmail.com>
2016-05-17 14:03:25 +0200

    composer + medoo + Data.php

---
+++ php/Data.php
@@ -0,0 +1,43 @@
+<?php
+require 'vendor/autoload.php';
+class Data {
+    private static $singletonInstance;
+    private function __construct(){
+        
+    }
+    public function getSingletonInstance(){
+        if(!self::$singletonInstance){
+            self::$singletonInstance = new self();
+        }
+        return self::$singletonInstance;
+    }
+}
+
+abstract class Source{
+    abstract public function search($tip);
+    abstract public function getFile($dir);
+    abstract public function listFiles($dir);
+}
+
+class Sour_MySQL extends Source{
+    private $db;
+    public function __construct($dbname,$server,$username,$password,$charset='utf8'){
+        $db = new medoo([
+            'database_type' => 'mysql',
+            'database_name' => $dbname,
+            'server' => $server,
+            'username' => $username,
+            'password' => $password,
+            'charset' => $charset
+        ]);
+    }
+    public function search($tip){
+        
+    }
+    public function getFile($dir){
+        
+    }
+    public function listFiles($dir){
+        
+    }
+}
