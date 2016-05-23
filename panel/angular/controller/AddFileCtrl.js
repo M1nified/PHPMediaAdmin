9c899ea3647c97786f4286d84ce515b76b7584b7
M!nified <gora.pub@gmail.com>
2016-05-23 14:35:09 +0200

    file upload

--- panel/angular/controller/AddFileCtrl.js
+++ panel/angular/controller/AddFileCtrl.js
@@ -7,7 +7,35 @@ app.controller('AddFileCtrl',['$scope',function($scope){
         console.log('AddFileCtrl_addFileSpot');
         files.push({});
     };
+    let uploadFile = function AddFileCtrl_uploadFile(evt,index){
+        evt.preventDefault();
+        console.log('AddFileCtrl_uploadFile');
+        let file = files[index];
+        let filesarr = evt.target.files.files;
+        file.files = filesarr;
+        console.log(files)
+        sendFile(file);
+    };
+    let sendFile = function AddFileCtrl_sendFile(file){
+         console.log(file);  
+         let fd = new FormData();
+         fd.append('filename',file.filename || '');
+         fd.append('keywords',file.keywords || '');
+         for(let i=0;i<file.files.length;i++){
+             fd.append('files[]',file.files[i],file.files[i].name);
+         }
+         console.log(fd);
+         let xhr = new XMLHttpRequest();
+         xhr.open('POST','action/addFiles.php',true);
+        //  xhr.setRequestHeader('Content-type', 'multipart/form-data; charset=utf-8');
+         xhr.onload = function(){
+             console.log(xhr.status);
+             console.log(xhr.response);
+         }
+         xhr.send(fd);
+    };
     
     $scope.files = files;
     $scope.addFileSpot = addFileSpot;
+    $scope.uploadFile = uploadFile;
 }])
--- panel/template/add.html.php
+++ panel/template/add.html.php
@@ -7,26 +7,26 @@ require_once realpath('~/../../../php/autoload.php');
     <button class="btn btn-primary navbar-btn" ng-click="addFileSpot()">Add file</button>
   </div>
   <div class="">
-    <form class="" action="index.html" method="post">
       <div class="panel panel-default" ng-repeat="file in files">
+        <form class="" method="post" ng-submit="uploadFile($event,$index)">
         <div class="panel-body">
             <div class="form-group input-group">
               <span class="input-group-addon" id="filename{{$index}}"><?php echo $GLOBALS['PMA_CONFIG']['files_location']; ?></span>
-              <input type="text" name="file_name" class="form-control" placeholder="File name" aria-describedby="filename{{$index}}" ng-model="file.filename">
+              <input type="text" name="file_name" class="form-control" placeholder="File name" aria-describedby="filename{{$index}}" ng-model="file.filename" required>
             </div>
             <div class="form-group input-group">
               <span class="input-group-addon" id="file{{$index}}">File: </span>
-              <input type="file" name="files" class="form-control" aria-describedby="file{{$index}}" ng-model="file.file" multiple>
+              <input type="file" name="files" class="form-control" aria-describedby="file{{$index}}" ng-model="file.files" multiple required>
             </div>
             <div class="form-group input-group">
               <span class="input-group-addon" id="file{{$index}}">Keywords: </span>
-              <input type="text" name="keywords" class="form-control" placeholder="Keywords (space separated)" aria-describedby="file{{$index}}" ng-model="file.keywords">
+              <input type="text" name="keywords" class="form-control" placeholder="Keywords (space separated)" aria-describedby="file{{$index}}" ng-model="file.keywords" required>
             </div>
-            <input type="submit" class="btn btn-success" value="Upload">
             <button class="btn btn-danger" ng-click="files.splice($index,1)">Cancel</button>
+            <input type="submit" class="btn btn-success" value="Upload">
         </div>
       </div>
-      <button class="btn btn-success" ng-show="files.length>0">Upload all files</button>
     </form>
+    <button class="btn btn-success" ng-show="files.length>0">Upload all files</button>
   </div>
 </div>
