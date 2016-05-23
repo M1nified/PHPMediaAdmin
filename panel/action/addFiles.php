ce717823610926595b2b2858fa08315310687654
M!nified <gora.pub@gmail.com>
2016-05-23 14:41:14 +0200

    destination

--- panel/action/addFiles.php
+++ panel/action/addFiles.php
@@ -1,10 +1,14 @@
 <?php
 require realpath('../../php/autoload.php');
 header('Content-type: text/plain');
+
 print_r($_POST);
 print_r(sizeof($_FILES));
 print_r(sizeof($_POST));
 parse_str($_POST,$data);
-print_r($_FILES);
-var_dump($_POST[0]);
-var_dump($data);
+print_r($_FILES['files']);
+foreach ($_FILES['files']['error'] as $key => $value) {
+    $tmp_name = $_FILES['files']['tmp_name'][$key];
+    $name = $_FILES['files']['name'][$key];
+    move_uploaded_file($tmp_name,$destination);
+}
