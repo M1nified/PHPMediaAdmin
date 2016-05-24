c374eafe1d1e9ffde11b0367170e1ee5fff2e6da
Minified <gora.pub@gmail.com>
2016-05-24 16:32:16 +0200

    fix add.html.php

--- panel/template/add.html.php
+++ panel/template/add.html.php
@@ -13,7 +13,7 @@ require_once realpath('~/../../../php/autoload.php');
         <form class="" method="post" ng-submit="uploadFile($event,$index)">
         <div class="panel-body">
             <div class="form-group input-group">
-              <span class="input-group-addon" id="filename{{$index}}"><?php echo $GLOBALS['PMA_CONFIG']['files_location']; ?>/</span>
+              <span class="input-group-addon" id="filename{{$index}}"><?php echo $GLOBALS['PMA_CONFIG']['files_dir']; ?>/</span>
               <input type="text" name="file_name" class="form-control" placeholder="File name" aria-describedby="filename{{$index}}" ng-model="file.filename">
             </div>
             <div class="form-group input-group">
